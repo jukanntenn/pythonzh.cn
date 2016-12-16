275389660df594e548f462e1f13058a1773b470e
yangxg <zmrenwu@163.com>
2016-12-16 15:06:29 +0800

    Display all categories in home page

--- forum/templatetags/forum_tags.py
+++ forum/templatetags/forum_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.db.models import Count
 
+from categories.models import Category
 from ..models import Post
 
 register = template.Library()
@@ -9,3 +10,13 @@ register = template.Library()
 @register.simple_tag
 def get_popular_posts():
     return Post.objects.all().annotate(num_replies=Count('replies')).order_by('-num_replies')[:6]
+
+
+@register.simple_tag
+def get_categories():
+    return Category.objects.filter(parent__isnull=True)
+
+
+@register.simple_tag
+def get_quick_categories():
+    return Category.objects.filter(parent__isnull=False)
