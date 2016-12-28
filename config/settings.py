08ecbd085ba3f15ca996462df6b4260a50763f8f
guoqiao <guoqiao@gmail.com>
2016-12-28 21:14:37 +1300

    Change db.sqlite3 path to BASE_DIR and ignore it
    It's unnecessary to put db.sqlite3 in a `database` directory.
    Since when developers clone this repo and try to run it for first time,
    there will be an error since the `database` dir does not exist.

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
