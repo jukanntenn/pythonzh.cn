3787c9768c58d77b7c6e640a3a8fbf370c915b22
guoqiao <guoqiao@gmail.com>
2016-12-28 21:48:00 +1300

    remove password validators which is very annoying for dev

--- config/settings.py
+++ config/settings.py
@@ -99,28 +99,6 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    # Translate error in django
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    # },
-    {
-        'NAME': 'users.password_validation.NumericPasswordValidator',
-    },
-]
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
